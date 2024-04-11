c7cfcbaeeeffb61071f842d4f35a20f5d4e292af
Colum Kelly <columk1@users.noreply.github.com>
2024-04-11 00:39:34 -0700

    Format and styling, remove fixed footer for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -9,35 +9,38 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>Squamish Aviation Dashboard&nbsp;</p>
-        <div>
+        {/* <div>
           <a
             href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
             target='_blank'
             rel='noopener noreferrer'
           >
             By columk
-            {/* <Image
+            <Image
               src='/vercel.svg'
               alt='Vercel Logo'
               className={styles.vercelLogo}
               width={100}
               height={24}
               priority
-            /> */}
+            />
           </a>
-        </div>
+        </div> */}
       </div>
 
       <WindGraph />
 
-      <iframe
-        width='450'
-        height='210'
-        src='https://chiefcam.com/embed/live'
-        frameBorder='0'
-        allow='autoplay; encrypted-media; fullscreen; picture-in-picture'
-        allowFullScreen
-      ></iframe>
+      <div className={styles.iFrameWrapper}>
+        <iframe
+          className={styles.iFrame}
+          width='450'
+          height='210'
+          src='https://chiefcam.com/embed/live'
+          frameBorder='0'
+          allow='autoplay; encrypted-media; fullscreen; picture-in-picture'
+          allowFullScreen
+        ></iframe>
+      </div>
 
       <Image
         src={`https://canadarasp.com/windgrams-data/oneDay/${date}/hrdpswindgram20.png`}
