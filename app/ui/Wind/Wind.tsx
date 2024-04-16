7d058192b1f2f60bbe8960517a56fc531d73a428
Colum Kelly <columk1@users.noreply.github.com>
2024-04-16 14:12:49 -0700

    Feat: Fetch on 2 minute interval

--- app/ui/Wind/Wind.tsx
+++ app/ui/Wind/Wind.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import testData from '@/app/lib/testData.json'
 // import WindGraph from '@/app/ui/WindGraph/WindGraph'
 import GondolaWX from '@/app/ui/GondolaWX/GondolaWX'
@@ -5,6 +7,7 @@ import WXCard from '@/app/ui/WXCard/WXCard'
 import styles from './Wind.module.css'
 import { fetchWindGraph } from '@/app/lib/actions'
 import dynamic from 'next/dynamic'
+import { useEffect, useState } from 'react'
 
 type ChartData = {
   wind_avg_data: number[][]
@@ -14,25 +17,31 @@ type ChartData = {
   last_wind_dir_text: string
 } | null
 
-const Wind = async () => {
-  const WindGraph = dynamic(() => import('@/app/ui/WindGraph/WindGraph'), {
-    ssr: false,
-  })
-  // const [data, setData] = useState<ChartData>(null)
-  // const fetchData = async () => {
-  const json = process.env.NODE_ENV === 'development' ? testData.wind : await fetchWindGraph()
+const WindGraph = dynamic(() => import('@/app/ui/WindGraph/WindGraph'), {
+  ssr: false,
+})
 
-  // const json = await fetchWindGraph()
-  // console.log(json)
-  const data = {
-    wind_avg_data: json.wind_avg_data.slice(0, -1) as number[][],
-    wind_gust_data: json.wind_gust_data.slice(0, -1) as number[][],
-    wind_lull_data: json.wind_lull_data.slice(0, -1) as number[][],
-    wind_dir_data: json.wind_dir_data.slice(0, -1) as number[][],
-    last_wind_dir_text: json.last_ob_dir_txt as string,
-  }
+const Wind = () => {
+  const [data, setData] = useState<ChartData>(null)
 
-  // fetchData()
+  useEffect(() => {
+    const fetchData = async () => {
+      const json = process.env.NODE_ENV === 'development' ? testData.wind : await fetchWindGraph()
+
+      setData({
+        wind_avg_data: json.wind_avg_data.slice(0, -1) as number[][],
+        wind_gust_data: json.wind_gust_data.slice(0, -1) as number[][],
+        wind_lull_data: json.wind_lull_data.slice(0, -1) as number[][],
+        wind_dir_data: json.wind_dir_data.slice(0, -1) as number[][],
+        last_wind_dir_text: json.last_ob_dir_txt as string,
+      })
+    }
+    fetchData()
+
+    const interval = setInterval(fetchData, 120000)
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <>
       <div className={styles.flexContainer}>
