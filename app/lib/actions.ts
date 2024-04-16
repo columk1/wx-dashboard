7b1777d38b65fea959fda834bbb9b4aa1b573bc2
Colum Kelly <columk1@users.noreply.github.com>
2024-04-16 12:43:24 -0700

    Add fetch actions for wx data

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,12 +1,18 @@
-const fetchWindGraph = async () => {
-  const endpoint = process.env.VITE_WINDGRAPH_URL
+'use server'
+
+const fetchData = async (endpoint?: string, revalidateLifetime = 0, tags?: string[]) => {
   if (!endpoint) return null
   try {
-    const response = await fetch(endpoint)
+    const response = revalidateLifetime
+      ? await fetch(endpoint, { next: { revalidate: revalidateLifetime, tags: tags } })
+      : await fetch(endpoint)
     return response.json()
   } catch (error) {
     console.log(error)
   }
 }
 
-export { fetchWindGraph }
+const fetchWindGraph = async () => fetchData(process.env.WINDGRAPH_URL)
+const fetchGondolaData = async () => fetchData(process.env.GONDOLA_API_URL, 10)
+
+export { fetchWindGraph, fetchGondolaData }
