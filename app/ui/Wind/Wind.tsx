77425d97def3f32fce3c425ec073e4d1455f8c7c
Colum Kelly <columk1@users.noreply.github.com>
2024-04-16 18:02:23 -0700

    Move fetching for gondola into wind component

--- app/ui/Wind/Wind.tsx
+++ app/ui/Wind/Wind.tsx
@@ -1,34 +1,33 @@
 'use client'
 
 import testData from '@/app/lib/testData.json'
-// import WindGraph from '@/app/ui/WindGraph/WindGraph'
 import GondolaWX from '@/app/ui/GondolaWX/GondolaWX'
 import WXCard from '@/app/ui/WXCard/WXCard'
 import styles from './Wind.module.css'
 import { fetchWindGraph } from '@/app/lib/actions'
 import dynamic from 'next/dynamic'
 import { useEffect, useState } from 'react'
-
-type ChartData = {
-  wind_avg_data: number[][]
-  wind_gust_data: number[][]
-  wind_lull_data: number[][]
-  wind_dir_data: number[][]
-  last_wind_dir_text: string
-} | null
+import { ChartData, WXCardData } from '@/app/lib/definitions'
 
 const WindGraph = dynamic(() => import('@/app/ui/WindGraph/WindGraph'), {
   ssr: false,
 })
 
+const getWindDirectionText = (windDirection: number) => {
+  // prettier-ignore
+  const directions = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW']
+  return directions[Math.round(windDirection / 22.5)]
+}
+
 const Wind = () => {
-  const [data, setData] = useState<ChartData>(null)
+  const [spitData, setSpitData] = useState<ChartData>(null)
+  const [gondolaData, setGondolaData] = useState<WXCardData>(null)
 
   useEffect(() => {
     const fetchData = async () => {
       const json = process.env.NODE_ENV === 'development' ? testData.wind : await fetchWindGraph()
 
-      setData({
+      setSpitData({
         wind_avg_data: json.wind_avg_data.slice(0, -1) as number[][],
         wind_gust_data: json.wind_gust_data.slice(0, -1) as number[][],
         wind_lull_data: json.wind_lull_data.slice(0, -1) as number[][],
@@ -42,23 +41,44 @@ const Wind = () => {
     return () => clearInterval(interval)
   }, [])
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const json = process.env.NODE_ENV === 'development' ? testData.gondola : await fetchData()
+      // console.log(json)
+      if (!json) return
+      setGondolaData({
+        windDirection: Math.round(json.observations[0].winddir),
+        windSpeed: Math.round(json.observations[0].metric.windSpeed),
+        windGusts: Math.round(json.observations[0].metric.windGust),
+        windDirectionText: getWindDirectionText(json.observations[0].winddir),
+      })
+      // setLoading(false)
+    }
+    fetchData()
+
+    const interval = setInterval(fetchData, 10000)
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <>
       <div className={styles.flexContainer}>
-        {data && (
-          <WXCard
-            title='Spit'
-            data={{
-              windSpeed: Math.round(data?.wind_avg_data[data?.wind_avg_data.length - 1][1]),
-              windDirection: data?.wind_dir_data[data?.wind_avg_data.length - 1][1],
-              windGusts: Math.round(data?.wind_gust_data[data?.wind_avg_data.length - 1][1]),
-              windDirectionText: data?.last_wind_dir_text,
-            }}
-          />
-        )}
-        <GondolaWX />
+        <WXCard
+          title='Spit'
+          data={
+            spitData && {
+              windSpeed: Math.round(spitData?.wind_avg_data[spitData?.wind_avg_data.length - 1][1]),
+              windDirection: spitData?.wind_dir_data[spitData?.wind_avg_data.length - 1][1],
+              windGusts: Math.round(
+                spitData?.wind_gust_data[spitData?.wind_avg_data.length - 1][1]
+              ),
+              windDirectionText: spitData?.last_wind_dir_text,
+            }
+          }
+        />
+        <WXCard title='Gondola' data={gondolaData} />
       </div>
-      <WindGraph data={data} />
+      <WindGraph data={spitData} />
     </>
   )
 }
