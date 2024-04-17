12a1428691c2cf17708bbbbe76cf4318fe2eb5ac
Colum Kelly <columk1@users.noreply.github.com>
2024-04-17 00:35:00 -0700

    Remove unused cacheing syntax

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,11 +1,9 @@
 'use server'
 
-const fetchData = async (endpoint?: string, revalidateLifetime = 0, tags?: string[]) => {
+const fetchData = async (endpoint?: string) => {
   if (!endpoint) return null
   try {
-    const response = revalidateLifetime
-      ? await fetch(endpoint, { next: { revalidate: revalidateLifetime, tags: tags } })
-      : await fetch(endpoint)
+    const response = await fetch(endpoint)
     return response.json()
   } catch (error) {
     console.log(error)
@@ -13,6 +11,6 @@ const fetchData = async (endpoint?: string, revalidateLifetime = 0, tags?: strin
 }
 
 const fetchWindGraph = async () => fetchData(process.env.SPIT_WINDMETER_API)
-const fetchGondolaData = async () => fetchData(process.env.GONDOLA_WINDMETER_API, 10)
+const fetchGondolaData = async () => fetchData(process.env.GONDOLA_WINDMETER_API)
 
 export { fetchWindGraph, fetchGondolaData }
