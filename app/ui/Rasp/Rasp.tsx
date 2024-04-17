aac199aa0f52ba4fa1f8ee69f56b4777d125cc18
Colum Kelly <columk1@users.noreply.github.com>
2024-04-16 18:04:46 -0700

    Preload next site

--- app/ui/Rasp/Rasp.tsx
+++ app/ui/Rasp/Rasp.tsx
@@ -20,16 +20,19 @@ const twoDay = 'twoDay'
 const periods = [today, tomorrow, twoDay]
 
 const Rasp = () => {
-  const [site, setSite] = useState('20')
+  const [siteIndex, setSiteIndex] = useState(0)
   const [periodIndex, setPeriodIndex] = useState(0)
 
   const period = periods[periodIndex]
 
-  const getNextPeriod = (currentIndex: number) => (currentIndex + 1) % periods.length
+  const getNextIndex = (array: any[], currentIndex: number) => (currentIndex + 1) % periods.length
 
-  const cyclePeriod = () => setPeriodIndex((prev) => getNextPeriod(prev))
+  const getNextItem = (array: any[], currentIndex: number) =>
+    array[(currentIndex + 1) % periods.length]
 
-  const src = `https://canadarasp.com/windgrams-data/${period}/hrdpswindgram${site}.png`
+  const cyclePeriod = () => setPeriodIndex((prev) => getNextIndex(periods, prev))
+
+  const src = `https://canadarasp.com/windgrams-data/${period}/hrdpswindgram${sites[siteIndex][1]}.png`
 
   return (
     <>
@@ -42,10 +45,23 @@ const Rasp = () => {
             height={450}
             className={styles.raspImg}
           />
+          {/* Preload next period of current site */}
           <Image
-            src={`https://canadarasp.com/windgrams-data/${getNextPeriod(
+            src={`https://canadarasp.com/windgrams-data/${getNextItem(
+              periods,
               periodIndex
-            )}/hrdpswindgram${site}.png`}
+            )}/hrdpswindgram${sites[siteIndex][1]}.png`}
+            alt={'Preload Next Rasp Windgram'}
+            width={450}
+            height={450}
+            className={styles.raspImg + ' sr-only'}
+          />
+          {/* Preload next site with current period */}
+          <Image
+            src={`https://canadarasp.com/windgrams-data/${period}/hrdpswindgram${getNextItem(
+              sites,
+              siteIndex
+            )}.png`}
             alt={'Preload Next Rasp Windgram'}
             width={450}
             height={450}
@@ -53,11 +69,11 @@ const Rasp = () => {
           />
         </button>
         <div className={styles.rasp}>
-          {sites.map((e) => (
+          {sites.map((e, i) => (
             <button
               key={e[1]}
-              onClick={() => setSite(e[1])}
-              className={`${styles.raspBtn} ${site === e[1] ? styles.active : ''}`}
+              onClick={() => setSiteIndex(i)}
+              className={`${styles.raspBtn} ${siteIndex === i ? styles.active : ''}`}
             >
               <p>{e[0]}</p>
             </button>
