07de1e922425ab2c5f1d1cb33294450c5f373f36
Colum Kelly <columk1@users.noreply.github.com>
2024-04-16 18:00:41 -0700

    Add type definitions

---
+++ app/lib/definitions.ts
@@ -0,0 +1,14 @@
+export type ChartData = {
+  wind_avg_data: number[][]
+  wind_gust_data: number[][]
+  wind_lull_data: number[][]
+  wind_dir_data: number[][]
+  last_wind_dir_text: string
+} | null
+
+export type WXCardData = {
+  windDirection: number
+  windSpeed: number
+  windGusts: number
+  windDirectionText: string
+} | null
