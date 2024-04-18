8ba05d3cf24441d7973bf05174c7e57aac0e7380
Colum Kelly <columk1@users.noreply.github.com>
2024-04-18 00:23:14 -0700

    Add favicon, icons, update manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,13 +7,24 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Chief Lap Copilot',
   description: 'A dashboard for local pilots.',
-  // icons: [
-  //   {
-  //     rel: 'icon',
-  //     type: 'image/x-icon',
-  //     url: '/favicon.ico',
-  //   },
-  // ],
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      url: '/icons/icon.ico',
+    },
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      url: '/icons/icon.svg',
+      sizes: 'any',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/icons/apple-touch-icon.png',
+    },
+  ],
   metadataBase: new URL('https://chieflap.vercel.app'),
 }
 
