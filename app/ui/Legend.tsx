ead8857873e2abdb91d59134c41f10f21f21f2f1
Colum Kelly <columk1@users.noreply.github.com>
2024-04-21 16:21:28 -0700

    Feat: Custom legend component

---
+++ app/ui/Legend.tsx
@@ -0,0 +1,120 @@
+const Legend = () => {
+  return (
+    <div
+      className='recharts-legend-wrapper'
+      style={{
+        width: '100%',
+        fontSize: '0.75rem',
+        marginTop: '5px',
+      }}
+    >
+      <ul
+        className='recharts-default-legend'
+        style={{ padding: '0px', margin: '0px, auto', textAlign: 'center' }}
+      >
+        <li
+          className='recharts-legend-item legend-item-0'
+          style={{ display: 'inline-block', marginRight: '10px' }}
+        >
+          <svg
+            className='recharts-surface'
+            width='8'
+            height='8'
+            viewBox='0 0 32 32'
+            style={{ display: 'inline-block', verticalAlign: 'middle', marginRight: '4px' }}
+          >
+            <title></title>
+            <desc></desc>
+            <path
+              fill='rgb(29, 145, 160)'
+              cx='16'
+              cy='16'
+              className='recharts-symbols'
+              transform='translate(16, 16)'
+              d='M16,0A16,16,0,1,1,-16,0A16,16,0,1,1,16,0'
+            ></path>
+          </svg>
+          <span className='recharts-legend-item-text' style={{ color: 'rgb(29, 145, 160)' }}>
+            Avg
+          </span>
+        </li>
+        <li
+          className='recharts-legend-item legend-item-1'
+          style={{ display: 'inline-block', marginRight: '10px' }}
+        >
+          <svg
+            className='recharts-surface'
+            width='8'
+            height='8'
+            viewBox='0 0 32 32'
+            style={{ display: 'inline-block', verticalAlign: 'middle', marginRight: '4px' }}
+          >
+            <title></title>
+            <desc></desc>
+            <path
+              fill='#f84c56'
+              cx='16'
+              cy='16'
+              className='recharts-symbols'
+              transform='translate(16, 16)'
+              d='M16,0A16,16,0,1,1,-16,0A16,16,0,1,1,16,0'
+            ></path>
+          </svg>
+          <span className='recharts-legend-item-text' style={{ color: 'rgb(248, 76, 86)' }}>
+            Gust
+          </span>
+        </li>
+        <li
+          className='recharts-legend-item legend-item-2'
+          style={{ display: 'inline-block', marginRight: '10px' }}
+        >
+          <svg
+            className='recharts-surface'
+            width='8'
+            height='8'
+            viewBox='0 0 32 32'
+            style={{ display: 'inline-block', verticalAlign: 'middle', marginRight: '4px' }}
+          >
+            <title></title>
+            <desc></desc>
+            <path
+              fill='#0f6b8a'
+              cx='16'
+              cy='16'
+              className='recharts-symbols'
+              transform='translate(16, 16)'
+              d='M16,0A16,16,0,1,1,-16,0A16,16,0,1,1,16,0'
+            ></path>
+          </svg>
+          <span className='recharts-legend-item-text' style={{ color: 'rgb(15, 107, 138)' }}>
+            Lull
+          </span>
+        </li>
+        <li
+          className='recharts-legend-item legend-item-2'
+          style={{ display: 'inline-block', marginRight: '10px' }}
+        >
+          <svg
+            stroke='currentColor'
+            fill='#1d91a0'
+            strokeWidth='0'
+            version='1.2'
+            baseProfile='tiny'
+            viewBox='0 0 24 24'
+            height='15'
+            width='15'
+          >
+            <g transform={`rotate(0) translate(-3, 5)`}>
+              <path d='M10.368 19.102c.349 1.049 1.011 1.086 1.478.086l5.309-11.375c.467-1.002.034-1.434-.967-.967l-11.376 5.308c-1.001.467-.963 1.129.085 1.479l4.103 1.367 1.368 4.102z'></path>
+            </g>
+          </svg>
+          <span className='recharts-legend-item-text' style={{ color: '#1d91a0' }}>
+            Direction
+          </span>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Legend
--- app/ui/WindGraph/WindGraph.tsx
+++ app/ui/WindGraph/WindGraph.tsx
@@ -2,17 +2,21 @@
 
 import styles from './WindGraph.module.css'
 import { useCallback, useEffect, useRef, useState } from 'react'
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts'
+import Legend from '@/app/ui/Legend'
 import CustomXAxisTick from '@/app/ui/CustomXAxisTick'
 import Spinner from '@/app/ui/Spinner/Spinner'
 import { ChartData } from '@/app/lib/definitions'
 
 const WindGraph = ({ data }: { data: ChartData }) => {
   const containerRef = useRef<HTMLDivElement>(null)
+  const yAxisRef = useRef<SVGElement>(null)
   // const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     containerRef.current && (containerRef.current.scrollLeft = containerRef?.current?.scrollWidth)
+    const yAxis = document.querySelector('.recharts-yAxis')
+    yAxis instanceof SVGElement && (yAxis.style.transform = `translateX(0px)`)
   }, [data])
 
   const maxGust =
@@ -39,125 +43,129 @@ const WindGraph = ({ data }: { data: ChartData }) => {
       <Spinner />
     </div>
   ) : (
-    <div ref={containerRef} className={styles.container}>
-      <LineChart
-        id='wind-graph'
-        width={1600}
-        height={300}
-        data={data.wind_avg_data.map((e, i) => ({
-          time: e[0],
-          avg: Math.round(e[1]),
-          gust: Math.round(data.wind_gust_data[i][1]),
-          lull: Math.round(data.wind_lull_data[i][1]),
-          dir: data.wind_dir_data[i][1],
-        }))}
-        // data={data.wind_avg_data}
-        margin={{ top: 0, right: -10, bottom: 30, left: 20 }}
-      >
-        <CartesianGrid strokeDasharray='3 3' stroke='currentColor' opacity={0.3} />
-        <Legend
-          align='right'
-          iconSize={8}
-          iconType='circle'
-          wrapperStyle={{
-            fontSize: '0.75rem',
-            bottom: 20,
-            right: 50,
-            padding: 4,
-          }}
-          formatter={(value) => value[0].toUpperCase() + value.slice(1)}
-        />
-        <Tooltip
-          offset={50}
-          formatter={(value: number, name: string) => [
-            value + 'km/h',
-            name[0].toUpperCase() + name.slice(1),
-          ]}
-          label='time'
-          labelFormatter={(label) =>
-            new Date(label).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })
-          }
-          labelStyle={{ display: 'block', color: 'rgb(var(--background-start-rgb))' }}
-          contentStyle={{
-            padding: '0.5rem 0.75rem',
-            fontSize: '0.9rem',
-            backgroundColor: 'color-mix(in srgb, currentColor 90%, transparent)',
-            borderRadius: '5px',
-          }}
-          itemStyle={{ padding: '0.15rem' }}
-          itemSorter={(item) => {
-            switch (item.dataKey) {
-              case 'gust':
-                return 0
-              case 'avg':
-                return 1
-              default:
-                return 2
+    <div className={styles.wrapper}>
+      <div ref={containerRef} className={styles.container}>
+        <LineChart
+          id='wind-graph'
+          width={1600}
+          height={300}
+          data={data.wind_avg_data.map((e, i) => ({
+            time: e[0],
+            avg: Math.round(e[1]),
+            gust: Math.round(data.wind_gust_data[i][1]),
+            lull: Math.round(data.wind_lull_data[i][1]),
+            dir: data.wind_dir_data[i][1],
+          }))}
+          // data={data.wind_avg_data}
+          margin={{ top: 0, right: -10, bottom: 30, left: 20 }}
+        >
+          <CartesianGrid strokeDasharray='3 3' stroke='currentColor' opacity={0.3} />
+          {/* <Legend
+            align='right'
+            iconSize={8}
+            iconType='circle'
+            wrapperStyle={{
+              fontSize: '0.75rem',
+              bottom: 20,
+              right: 50,
+              padding: 4,
+            }}
+            formatter={(value) => value[0].toUpperCase() + value.slice(1)}
+          /> */}
+          <Tooltip
+            offset={50}
+            formatter={(value: number, name: string) => [
+              value + 'km/h',
+              name[0].toUpperCase() + name.slice(1),
+            ]}
+            label='time'
+            labelFormatter={(label) =>
+              new Date(label).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })
             }
-          }}
-        />
-        {/* Time X-Axis */}
-        <XAxis
-          xAxisId={0}
-          axisLine={false}
-          dataKey='time'
-          domain={['auto', 'auto']}
-          orientation='top'
-          scale='time'
-          tickFormatter={(time) =>
-            new Date(time).toLocaleString('en-US', { hour: '2-digit', hour12: false }) + 'h'
-          }
-          ticks={getTimeTicks()}
-          type='number'
-        />
-        {/* Wind Direction X-Axis */}
-        <XAxis
-          xAxisId={1}
-          dataKey='dir'
-          tickFormatter={(time) => ''}
-          tick={<CustomXAxisTick dirArray={data.wind_dir_data} />}
-          axisLine={false}
-          tickLine={false}
-          mirror={true}
-          tickMargin={-8}
-        />
-        <YAxis
-          domain={[0, (dataMax: number) => Math.ceil(dataMax / 10) * 10]}
-          padding={{ top: 0, bottom: 0 }}
-          ticks={Array.from(
-            { length: Math.ceil(maxGust / 20) }, // count multiples of 20
-            (_, index) => index * 20 // multiply each index by 20
-          )}
-          orientation='right'
-        />
-        <Line
-          type='monotone'
-          dataKey='avg'
-          stroke='#1d91a0'
-          dot={false}
-          activeDot={{ strokeWidth: 1, r: 4 }}
-          xAxisId={0}
-          isAnimationActive={false}
-        />
-        <Line
-          type='monotone'
-          dataKey='gust'
-          stroke='#f84c56'
-          dot={false}
-          activeDot={{ strokeWidth: 1, r: 4 }}
-          connectNulls={true}
-          isAnimationActive={false}
-        />
-        <Line
-          type='monotone'
-          dataKey='lull'
-          stroke='#0f6b8a'
-          dot={false}
-          activeDot={{ strokeWidth: 1, r: 4 }}
-          connectNulls={true}
-          isAnimationActive={false}
-        />
-      </LineChart>
+            labelStyle={{ display: 'block', color: 'rgb(var(--background-start-rgb))' }}
+            contentStyle={{
+              padding: '0.5rem 0.75rem',
+              fontSize: '0.9rem',
+              backgroundColor: 'color-mix(in srgb, currentColor 90%, transparent)',
+              borderRadius: '5px',
+            }}
+            itemStyle={{ padding: '0.15rem' }}
+            itemSorter={(item) => {
+              switch (item.dataKey) {
+                case 'gust':
+                  return 0
+                case 'avg':
+                  return 1
+                default:
+                  return 2
+              }
+            }}
+          />
+          {/* Time X-Axis */}
+          <XAxis
+            xAxisId={0}
+            axisLine={false}
+            dataKey='time'
+            domain={['auto', 'auto']}
+            orientation='top'
+            scale='time'
+            tickFormatter={(time) =>
+              new Date(time).toLocaleString('en-US', { hour: '2-digit', hour12: false }) + 'h'
+            }
+            ticks={getTimeTicks()}
+            type='number'
+          />
+          {/* Wind Direction X-Axis */}
+          <XAxis
+            xAxisId={1}
+            dataKey='dir'
+            tickFormatter={(time) => ''}
+            tick={<CustomXAxisTick dirArray={data.wind_dir_data} />}
+            axisLine={false}
+            tickLine={false}
+            mirror={true}
+            tickMargin={-8}
+          />
+          <YAxis
+            domain={[0, (dataMax: number) => Math.ceil(dataMax / 10) * 10]}
+            padding={{ top: 0, bottom: 0 }}
+            ticks={Array.from(
+              { length: Math.ceil(maxGust / 20) }, // count multiples of 20
+              (_, index) => index * 20 // multiply each index by 20
+            )}
+            orientation='right'
+          />
+          <Line
+            type='monotone'
+            dataKey='avg'
+            stroke='#1d91a0'
+            dot={false}
+            activeDot={{ strokeWidth: 1, r: 4 }}
+            xAxisId={0}
+            isAnimationActive={false}
+          />
+          <Line
+            type='monotone'
+            dataKey='gust'
+            stroke='#f84c56'
+            dot={false}
+            activeDot={{ strokeWidth: 1, r: 4 }}
+            connectNulls={true}
+            isAnimationActive={false}
+          />
+          <Line
+            type='monotone'
+            dataKey='lull'
+            stroke='#0f6b8a'
+            dot={false}
+            activeDot={{ strokeWidth: 1, r: 4 }}
+            connectNulls={true}
+            isAnimationActive={false}
+          />
+        </LineChart>
+      </div>
+      {/* Custom version of the generated legend, placed outside the scrollable container */}
+      <Legend />
     </div>
   )
 }
