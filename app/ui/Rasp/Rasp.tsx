5eae6afda56d3c3c6d06a57b60494009a9ef09b3
Colum Kelly <columk1@users.noreply.github.com>
2024-04-21 22:54:27 -0700

    Fix images not pre-loading

--- app/ui/Rasp/Rasp.tsx
+++ app/ui/Rasp/Rasp.tsx
@@ -14,11 +14,12 @@ const sites = [
 ]
 
 const now = new Date()
-const today = `oneDay/${format(now, 'yyyy-MM-dd')}`
-const tomorrow = `oneDay/${format(addDays(now, 1), 'yyyy-MM-dd')}`
-const twoDay = 'twoDay'
 
-const periods = [today, tomorrow, twoDay]
+const periods = [
+  ['Today', `oneDay/${format(now, 'yyyy-MM-dd')}`],
+  ['Tomorrow', `oneDay/${format(addDays(now, 1), 'yyyy-MM-dd')}`],
+  ['Two Day', 'twoDay'],
+]
 
 const Rasp = () => {
   const [siteIndex, setSiteIndex] = useState(0)
@@ -29,12 +30,12 @@ const Rasp = () => {
     if (currentDate.getDate() !== now.getDate()) setPeriodIndex((prev) => prev)
   }, [])
 
-  const period = periods[periodIndex]
+  const period = periods[periodIndex][1]
 
-  const getNextIndex = (array: any[], currentIndex: number) => (currentIndex + 1) % periods.length
+  const getNextIndex = (array: any[], currentIndex: number) => (currentIndex + 1) % array.length
 
   const getNextItem = (array: any[], currentIndex: number) =>
-    array[(currentIndex + 1) % periods.length]
+    array[(currentIndex + 1) % array.length]
 
   const cyclePeriod = () => setPeriodIndex((prev) => getNextIndex(periods, prev))
 
@@ -43,6 +44,17 @@ const Rasp = () => {
   return (
     <>
       <div className={styles.raspWrapper}>
+        <div className={styles.periodBtns}>
+          {periods.map((e, i) => (
+            <button
+              key={e[0]}
+              onClick={() => setPeriodIndex(i)}
+              className={`${styles.periodBtn} ${periodIndex === i ? styles.active : ''}`}
+            >
+              {e[0]}
+            </button>
+          ))}
+        </div>
         <button onClick={cyclePeriod}>
           <Image
             src={src}
@@ -50,40 +62,43 @@ const Rasp = () => {
             width={450}
             height={450}
             className={styles.raspImg}
+            priority
           />
           {/* Preload next period of current site */}
           <Image
-            src={`https://canadarasp.com/windgrams-data/${getNextItem(
-              periods,
-              periodIndex
-            )}/hrdpswindgram${sites[siteIndex][1]}.png`}
+            src={`https://canadarasp.com/windgrams-data/${
+              getNextItem(periods, periodIndex)[1]
+            }/hrdpswindgram${sites[siteIndex][1]}.png`}
             alt={'Preload Next Rasp Windgram'}
             width={450}
             height={450}
+            loading='eager'
             className={styles.raspImg + ' sr-only'}
           />
           {/* Preload next site with current period */}
           <Image
-            src={`https://canadarasp.com/windgrams-data/${period}/hrdpswindgram${getNextItem(
-              sites,
-              siteIndex
-            )}.png`}
+            src={`https://canadarasp.com/windgrams-data/${period}/hrdpswindgram${
+              getNextItem(sites, siteIndex)[1]
+            }.png`}
             alt={'Preload Next Rasp Windgram'}
             width={450}
             height={450}
+            loading='eager'
             className={styles.raspImg + ' sr-only'}
           />
         </button>
-        <div className={styles.rasp}>
-          {sites.map((e, i) => (
-            <button
-              key={e[1]}
-              onClick={() => setSiteIndex(i)}
-              className={`${styles.raspBtn} ${siteIndex === i ? styles.active : ''}`}
-            >
-              <p>{e[0]}</p>
-            </button>
-          ))}
+        <div className={styles.btnContainer}>
+          <div className={styles.raspBtns}>
+            {sites.map((e, i) => (
+              <button
+                key={e[0]}
+                onClick={() => setSiteIndex(i)}
+                className={`${styles.raspBtn} ${siteIndex === i ? styles.active : ''}`}
+              >
+                <p>{e[0]}</p>
+              </button>
+            ))}
+          </div>
         </div>
       </div>
     </>
