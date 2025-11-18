11cadc597016895d6e021e38d832c7ce43571b99
Colum Kelly <columk1@users.noreply.github.com>
2025-11-18 00:11:12 -0800

    fix: arrow position to center

--- app/ui/Arrow.tsx
+++ app/ui/Arrow.tsx
@@ -12,7 +12,7 @@ const Arrow = ({ size = '12', angle }: { size?: string; angle: number }) => {
       height={size}
       width={size}
     >
-      <g transform={`rotate(${angle + 135} 12 12.5)`}>
+      <g transform={`translate(0 1.2) rotate(${angle + 135} 12 12.5)`}>
         <path d='M10.368 19.102c.349 1.049 1.011 1.086 1.478.086l5.309-11.375c.467-1.002.034-1.434-.967-.967l-11.376 5.308c-1.001.467-.963 1.129.085 1.479l4.103 1.367 1.368 4.102z'></path>
       </g>
     </svg>
