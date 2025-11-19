d0e32403621ec6cc445d51eb16e0abfaaecf4708
Colum Kelly <columk1@users.noreply.github.com>
2025-11-19 15:27:41 -0800

    fix: alt preload to remove image warning and fix layout shift

--- app/ui/Rasp/Rasp.tsx
+++ app/ui/Rasp/Rasp.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import styles from './Rasp.module.css'
-import { useEffect, useReducer, useState } from 'react'
+import { useEffect, useMemo, useReducer, useState } from 'react'
 import { addDays, format } from 'date-fns'
 import { toZonedTime } from 'date-fns-tz'
 import sites from '@/app/lib/data/raspSites.json'
@@ -10,36 +10,50 @@ const getNextItem = (array: any[], currentIndex: number) => array[(currentIndex 
 
 const getPacificTimestamp = () => toZonedTime(Date.now(), 'America/Los_Angeles')
 
-const getTimeSuffix = (date: Date) =>
-  date.getDay() + date.getHours() > 6 && date.getHours() < 19 ? 'am' : 'pm'
+// const getTimeSuffix = (date: Date) =>
+//   date.getDay() + date.getHours() > 6 && date.getHours() < 19 ? 'am' : 'pm'
+
+const preloadImages = (srcs: string[]) => {
+  srcs.forEach((src) => {
+    const img = new Image()
+    img.src = src
+  })
+}
 
 const Rasp = () => {
-  // Get the current timestamp in Pacific Time
+  const [siteIndex, setSiteIndex] = useState(0)
+  const [periodIndex, setPeriodIndex] = useState(0)
+  const [src, setSrc] = useState<string | null>(null)
+  const [imageError, setImageError] = useState(false)
+
   const nowPT = getPacificTimestamp()
 
-  const periods = [
+  const periods = useMemo(() => [
     ['Today', `oneDay/${format(nowPT, 'yyyy-MM-dd')}`],
     ['Tomorrow', `oneDay/${format(addDays(nowPT, 1), 'yyyy-MM-dd')}`],
     ['Two Day', 'twoDay'],
-  ]
+  ], [nowPT])
 
-  const [siteIndex, setSiteIndex] = useState(0)
-  const [periodIndex, setPeriodIndex] = useState(0)
-  const [imageError, setImageError] = useState(false)
   const period = periods[periodIndex][1]
   const site = sites[siteIndex][1]
 
-  const [src, setSrc] = useState<string | null>(null)
-  // const src = `https://canadarasp.com/windgrams-data/${period}/hrdpswindgram${site}.png`
-  // const [src, setSrc] = useState(`https://canadarasp.com/windgrams-data/${period}/hrdpswindgram${site}.png`)
 
   const cyclePeriod = () => setPeriodIndex((prev) => (prev + 1) % periods.length)
 
   // Set the src on the client to prevent pre-rendering on the server (for caching/timing consistency)
   useEffect(() => {
-    //   // setSrc(`/api/windgrams?period=${period}&site=${sites[siteIndex][1]}`)
     setSrc(`https://canadarasp.com/windgrams-data/${period}/hrdpswindgram${site}.png`)
   }, [site, period])
+
+
+  // Preload next site and next period of to pre-empt RASP navigation
+  useEffect(() => {
+    const preloadImageSrcs = [
+      `https://canadarasp.com/windgrams-data/${getNextItem(periods, periodIndex)[1]}/hrdpswindgram${site}.png`,
+      `https://canadarasp.com/windgrams-data/${period}/hrdpswindgram${getNextItem(sites, siteIndex)[1]}.png`,
+    ]
+    preloadImages(preloadImageSrcs)
+  }, [period, periods, site, siteIndex, periodIndex])
 
   return (
     <>
@@ -56,45 +70,23 @@ const Rasp = () => {
           ))}
         </div>
         <button onClick={cyclePeriod}>
-          {imageError ? (
-            <div className={styles.error}>Keep Parawaiting</div>
-          ) : (
-            src && (
-              // eslint-disable-next-line @next/next/no-img-element
-              <img
-                src={src}
-                alt={'Rasp Windgram'}
-                className={styles.raspImg}
-                width={450}
-                height={450}
-                onError={() => setImageError(true)}
-              />
-            )
-          )}
-          {/* Preload next period of current site */}
-          <link
-            rel='preload'
-            as='image'
-            // Used with own caching layer
-            // href={`/api/windgrams?period=${getNextItem(periods, periodIndex)[1]}&site=${sites[siteIndex][1]}`}
-            // Used with Next/Image caching
-            // href={`https://canadarasp.com/windgrams-data/${getNextItem(periods, periodIndex)[1]}/hrdpswindgram${sites[siteIndex][1]}.png?${getTimeSuffix(nowPT)}`}
-            href={`https://canadarasp.com/windgrams-data/${
-              getNextItem(periods, periodIndex)[1]
-            }/hrdpswindgram${sites[siteIndex][1]}.png`}
-          />
-          {/* Preload next site with current period */}
-          <link
-            rel='preload'
-            as='image'
-            // Used with own caching layer
-            // href={`/api/windgrams?period=${periods[periodIndex][1]}&site=${getNextItem(sites, siteIndex)[1]}`}
-            // Used with Next/Image caching
-            // href={`https://canadarasp.com/windgrams-data/${period}/hrdpswindgram${getNextItem(sites, siteIndex)[1]}.png?${getTimeSuffix(nowPT)}`}
-            href={`https://canadarasp.com/windgrams-data/${period}/hrdpswindgram${
-              getNextItem(sites, siteIndex)[1]
-            }.png`}
-          />
+          <div className={styles.imgShared}>
+            {imageError ? (
+              <div className={styles.error}>Keep Parawaiting</div>
+            ) : (
+              src && (
+                // eslint-disable-next-line @next/next/no-img-element
+                <img
+                  src={src}
+                  alt={'Rasp Windgram'}
+                  className={styles.raspImg}
+                  width={450}
+                  height={450}
+                  onError={() => setImageError(true)}
+                />
+              )
+            )}
+          </div>
         </button>
         <div className={styles.btnContainer}>
           <div className={styles.raspBtns}>
